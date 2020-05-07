ae3b2870aca566e78d949209d246dcb383960bfe
Piotr Grabowski <grabowskipiotr1989@gmail.com>
2020-05-07 22:09:04 +0200

    Panel administracyjny - wyswietlanie widokow

---
+++ src/main/java/pl/coderslab/orderfood/controller/AdminController.java
@@ -0,0 +1,69 @@
+package pl.coderslab.orderfood.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pl.coderslab.orderfood.entity.Category;
+import pl.coderslab.orderfood.entity.Item;
+import pl.coderslab.orderfood.repository.CategoryRepository;
+import pl.coderslab.orderfood.repository.ItemRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/admin/item")
+public class AdminController {
+
+    private final ItemRepository itemRepository;
+    private final CategoryRepository categoryRepository;
+
+    public AdminController(ItemRepository itemRepository, CategoryRepository categoryRepository) {
+        this.itemRepository = itemRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @ModelAttribute("categories")
+    public List<Category> categories() {
+        return categoryRepository.findAll();
+    }
+
+    @ModelAttribute("items")
+    public List<Item> items() {
+        return itemRepository.findAll();
+    }
+
+    @GetMapping("/add")
+    public String addNewItem(Model model) {
+
+        model.addAttribute("item", new Item());
+        return "admin/product-form";
+    }
+
+    @PostMapping("/add")
+    public String addNewItemForm(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "redirect:/admin/item";
+    }
+
+    @GetMapping("")
+    public String listOfItems() {
+        return "/admin/item-list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteItem(@PathVariable long id) {
+        Optional<Item> item = itemRepository.findById(id);
+        item.ifPresent(itemRepository::delete);
+        return "redirect:/admin/item";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateItem(@PathVariable long id, Model model) {
+        Optional<Item> item = itemRepository.findById(id);
+        item.ifPresent(value -> model.addAttribute("item", value));
+        return "admin/product-form";
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/views/admin/menu-dashboard.jsp
@@ -0,0 +1,34 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<jsp:include page="../header.jsp"/>
+
+<body>
+<div>
+    <table id="menu">
+        <tr>
+            <th>Menu:</th>
+        </tr>
+
+        <tr>
+            <td><a href="...">Lista potraw</a></td>
+        </tr>
+
+        <tr>
+            <td><a href="...">Lista kategorii</a></td>
+        </tr>
+
+        <tr>
+            <td><a href="...">Dodaj potrawe</a></td>
+        </tr>
+
+        <tr>
+            <td><a href="...">Dodaj kategorie</a></td>
+        </tr>
+    </table>
+    <table id="items">
+
+    </table>
+</div>
+</body>
+</html>
